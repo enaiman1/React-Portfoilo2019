520dfc9fd42871e80d1262e6b46fa78427285a7a
Ericcentrik <enaiman1@gmail.com>
2019-05-17 11:35:20 -0400

    Updates

---
+++ precache-manifest.06e46dbc3ba92dbc9f2c9eebaf38e8cf.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1acd2a18fc439507c6dec50f3883435d",
+    "url": "/React-Portfoilo/index.html"
+  },
+  {
+    "revision": "012dc0e107e40f09577c",
+    "url": "/React-Portfoilo/static/css/main.2687b132.chunk.css"
+  },
+  {
+    "revision": "e2215de434ee15780e4f",
+    "url": "/React-Portfoilo/static/js/2.0b5e9773.chunk.js"
+  },
+  {
+    "revision": "012dc0e107e40f09577c",
+    "url": "/React-Portfoilo/static/js/main.cacc760a.chunk.js"
+  },
+  {
+    "revision": "1febe035d7c77e3b1fe0",
+    "url": "/React-Portfoilo/static/js/runtime~main.9a542d6f.js"
+  },
+  {
+    "revision": "884e523b00f6b1a72aa8460127dbcaed",
+    "url": "/React-Portfoilo/static/media/bioPic.884e523b.JPG"
+  },
+  {
+    "revision": "f51869e91f7caa0eec65c7b03b7620e3",
+    "url": "/React-Portfoilo/static/media/burger.f51869e9.jpeg"
+  },
+  {
+    "revision": "43bc8354f50be4339d25deea0710dd35",
+    "url": "/React-Portfoilo/static/media/cave.43bc8354.jpg"
+  },
+  {
+    "revision": "f46cf2db7e6df91a1ff500a201f0643b",
+    "url": "/React-Portfoilo/static/media/construction.f46cf2db.jpg"
+  },
+  {
+    "revision": "23e7faf1478ae047121e38c0ce703225",
+    "url": "/React-Portfoilo/static/media/contact2.23e7faf1.jpg"
+  },
+  {
+    "revision": "a5273f4f00de2c16609d2c9c3705106e",
+    "url": "/React-Portfoilo/static/media/curtains.a5273f4f.jpg"
+  },
+  {
+    "revision": "89969881f4b01eb91b07f1b55c504bf1",
+    "url": "/React-Portfoilo/static/media/dove.89969881.jpg"
+  },
+  {
+    "revision": "d84a699606399f515eca9c008d51a2dd",
+    "url": "/React-Portfoilo/static/media/friends.d84a6996.jpg"
+  },
+  {
+    "revision": "30c9a5cf3b54986ae8fe28fa44160c0e",
+    "url": "/React-Portfoilo/static/media/gbooks.30c9a5cf.jpg"
+  },
+  {
+    "revision": "fe330b5e9df906a1e8666180dcfde59f",
+    "url": "/React-Portfoilo/static/media/geometric.fe330b5e.jpg"
+  },
+  {
+    "revision": "6d583b8a9dc1e9ab9b6c45f1c3211307",
+    "url": "/React-Portfoilo/static/media/lantern.6d583b8a.gif"
+  },
+  {
+    "revision": "d8631c5e3e343c2d962091d98add4e88",
+    "url": "/React-Portfoilo/static/media/library.d8631c5e.jpg"
+  },
+  {
+    "revision": "55ed94fc536548921c88c45d7ca0c62c",
+    "url": "/React-Portfoilo/static/media/light3.55ed94fc.gif"
+  },
+  {
+    "revision": "f8a31586825d838970230a1d938cb2bb",
+    "url": "/React-Portfoilo/static/media/link.f8a31586.png"
+  },
+  {
+    "revision": "02557a9d3bcdf23f7f6fdce579a2d8fe",
+    "url": "/React-Portfoilo/static/media/logoWhite_shadow.02557a9d.png"
+  },
+  {
+    "revision": "16629d03735e264d9b0b9930362ba674",
+    "url": "/React-Portfoilo/static/media/patterns.16629d03.jpg"
+  },
+  {
+    "revision": "d652528e5b0ce319af2504c3c2439c6e",
+    "url": "/React-Portfoilo/static/media/pets.d652528e.jpeg"
+  },
+  {
+    "revision": "856fee9efdab72cca5abd422ca2c4715",
+    "url": "/React-Portfoilo/static/media/pubTrivia.856fee9e.jpg"
+  },
+  {
+    "revision": "05f5a6ed847af41e1ffca9c4262b6f95",
+    "url": "/React-Portfoilo/static/media/tracks.05f5a6ed.jpg"
+  }
+]);
